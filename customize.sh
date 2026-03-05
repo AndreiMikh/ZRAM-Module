68898c7d36026c630d82f4e80750fa23865c9fe2
AndreiMikh <andreimikhail0087@gmail.com>
2026-03-06 01:07:29 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,27 +1,22 @@
-# 自动获取模块名称与路径
+# Automatically Obtain the Module Name and Path
 MODNAME="$(basename "$MODPATH")"
 OLD_MODPATH="/data/adb/modules/$MODNAME"
 
-# 当前与旧模块 ZRAM 路径
+# The Current and Legacy Module ZRAM Paths
 ZRAM_DIR="$MODPATH/zram"
 OLD_ZRAM_DIR="$OLD_MODPATH/zram"
 
-ui_print "-------------"
-ui_print "  _____           _     ____ "
-ui_print " |  ___|   _ _ __| |   / ___|"
-ui_print " | |_ | | | | '__| |  | |    "
-ui_print " |  _|| |_| | |  | |__| |___ "
-ui_print " |_|   \__,_|_|  |_____\____|"
-ui_print "      FurLC ZRAM Module      "
-ui_print "-------------"
+ui_print "============="
+ui_print "ZRAM Module"
+ui_print "============="
 
-ui_print ">> 检查已安装模块的 zram 文件夹是否存在..."
+ui_print ">> Check if the ZRAM Folder for the Installed Module Exists..."
 
 if [ -d "$OLD_ZRAM_DIR" ]; then
-  ui_print ">> 已检测到旧模块 zram 文件夹，复制保留文件中..."
+  ui_print ">> The Old Module ZRAM Folder has been Detected, Copied to the Retained File..."
   mkdir -p "$ZRAM_DIR"
   cp -af "$OLD_ZRAM_DIR/." "$ZRAM_DIR/"
-  ui_print ">> 文件复制完成 ✅"
+  ui_print ">> ✅ File Copied"
 else
-  ui_print ">> 未检测到旧模块 zram 文件夹，跳过复制"
+  ui_print ">> The Old Module ZRAM Folder is Not Detected, Skipping Copying"
 fi
